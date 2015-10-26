3eda3122d6713cffd767a8934df489fa639ac2dc
bharatak <bharatak@thoughtworks.com>
2015-10-26 09:09:10 +0530

    Rahul,Bharat| Bahmni openerp rpm

--- bahmni-erp/scripts/postinstall.sh
+++ bahmni-erp/scripts/postinstall.sh
@@ -3,7 +3,19 @@
 # backing up existing startup script for openerp
 mv /etc/init.d/openerp /etc/init.d/openerp.bak
 # copying patched startup script for openerp
-cd /opt/bahmni-erp/bahmni-erp && cp openerp /etc/init.d/
+cp /opt/bahmni-erp/etc/openerp /etc/init.d/
+
+adduser openerp
+DIR="/var/run/openerp /var/log/openerp"
+for NAME in $DIR
+do
+    if [ ! -d $NAME ]; then
+    mkdir $NAME
+    chown openerp:openerp $NAME
+    fi
+done
+
+chkconfig openerp on
 
 # permissions
 chown -R openerp:openerp /opt/bahmni-erp
