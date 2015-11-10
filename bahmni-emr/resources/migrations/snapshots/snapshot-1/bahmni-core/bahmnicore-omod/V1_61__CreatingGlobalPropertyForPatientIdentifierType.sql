016b9909c47a1530fceac12f42b1dc43118fe014
shruthidipali <shruthidipali@gmail.com>
2015-11-10 17:32:32 +0530

    Revert "copying over snapshot migrations. removing the dependency on bahmni-environment."
    This reverts commit 11984b9733bbcfc9087b2d85a064f55b09e88122.

--- bahmni-emr/resources/migrations/snapshots/snapshot-1/bahmni-core/bahmnicore-omod/V1_61__CreatingGlobalPropertyForPatientIdentifierType.sql
+++
@@ -1,2 +0,0 @@
-select uuid from patient_identifier_type where name = 'Bahmni Id' into @uuid;
-UPDATE global_property SET property_value = @uuid WHERE property='emr.primaryIdentifierType';
--- bahmni-emr/resources/migrations/snapshots/snapshot-1/bahmni-core/bahmnicore-omod/V1_80__AddConceptNumericProc.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE add_concept_numeric (concept_id INT,
-							  low_normal DOUBLE,
-							  hi_normal DOUBLE,
-							  units VARCHAR(50))
-BEGIN
-  INSERT INTO concept_numeric (concept_id, low_normal, hi_normal, units) values (concept_id, low_normal, hi_normal, units);
-END;
-
