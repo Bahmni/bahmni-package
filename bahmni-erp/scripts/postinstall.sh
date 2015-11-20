296f5ba3e43f686ceb353f79c68445c7245f24a3
bharatak <bharatak@thoughtworks.com>
2015-11-20 13:09:05 +0530

    Bharat| Cleaned up bahmni-erp post install script and fixed one issue.

--- bahmni-erp/scripts/postinstall.sh
+++ bahmni-erp/scripts/postinstall.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
 export BAHMNI_ERP=/opt/bahmni-erp
-cp -f $BAHMNI_ERP/etc/openerp /etc/init.d/openerp
 
-adduser openerp
-DIR="/var/run/openerp /var/log/openerp"
-for NAME in $DIR
-do
-    if [ ! -d $NAME ]; then
-    mkdir $NAME
-    chown openerp:openerp $NAME
-    fi
-done
+manage_permissions(){
+    adduser openerp
+    chown -R openerp:openerp $BAHMNI_ERP
+    chown openerp:openerp /var/log/openerp
+    chown openerp:openerp /var/run/openerp
+}
 
-chkconfig openerp on
+install_openerp(){
+    cd $BAHMNI_ERP
+    tar -xvzf openerp-7.0-20130301-002301.tar.gz
+    cd openerp-7.0-20130301-002301
+    sudo python setup.py -q install
+    cp openerp-server $BAHMNI_ERP
+    cp install/openerp-server.conf $BAHMNI_ERP/etc
+    cp -r $BAHMNI_ERP/bahmni-addons/* /usr/lib/python2.6/site-packages/openerp-7.0_20130301_002301-py2.6.egg/openerp/addons
+    rm -rf $BAHMNI_ERP/openerp-7.0-20130301-002301
+    rm -rf $BAHMNI_ERP/openerp-7.0-20130301-002301.tar.gz
+}
 
-# permissions
-chown -R openerp:openerp $BAHMNI_ERP
+link_directories(){
+    ln -s $BAHMNI_ERP/etc /etc/openerp
+    chown -R openerp:openerp /etc/openerp
+}
 
-# installing openerp
-cd $BAHMNI_ERP
-tar -xvzf openerp-7.0-20130301-002301.tar.gz
-cd openerp-7.0-20130301-002301
-sudo python setup.py -q install
-cp openerp-server $BAHMNI_ERP
-cp install/openerp-server.conf $BAHMNI_ERP
-cp -r $BAHMNI_ERP/bahmni-addons/* /usr/lib/python2.6/site-packages/openerp-7.0_20130301_002301-py2.6.egg/openerp/addons
+manage_config(){
+    chkconfig openerp on
+    cp -f $BAHMNI_ERP/etc/openerp /etc/init.d/openerp
+}
 
-rm -rf $BAHMNI_ERP/openerp-7.0-20130301-002301
-rm -rf $BAHMNI_ERP/openerp-7.0-20130301-002301.tar.gz
+manage_permissions
+install_openerp
+link_directories
+manage_config
