f16647969334d539069a5545a3fd371625b6bf84
Preethi <preethi.ms34@gmail.com>
2015-11-24 11:58:39 +0530

    Preethi, Shruthi | Changed mrs sql dump to func test dump and removed openmrs dependency from bahmni-emr

--- openmrs/resources/initDB.sh
+++ openmrs/resources/initDB.sh
@@ -4,7 +4,6 @@ set -e -x
 RESULT=`mysql -uroot -ppassword --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
 if [ "$RESULT" != "openmrs" ]; then
     echo "openmrs database not found... Restoring a base dump"
-    mysql -uroot -ppassword -e "create database openmrs"
     mysql -uroot -ppassword openmrs < scripts/mrs_base.sql
     mysql -uroot -ppassword -e "CREATE USER 'openmrs-user'@'localhost' IDENTIFIED BY '*';
     GRANT ALL PRIVILEGES ON openmrs.* TO 'openmrs-user'@'localhost' identified by 'password'  WITH GRANT OPTION;"
