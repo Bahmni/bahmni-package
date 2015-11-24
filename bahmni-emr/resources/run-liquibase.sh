0b2d883574b8a500247c21e22c0570961fad699c
Preethi <preethi.ms34@gmail.com>
2015-11-24 20:34:25 +0530

    Preethi, Shruthi | Used OPENMRS_DB_SERVER variable while restoring mrs dump and removed 'snapshot' from openmrs version number

--- bahmni-emr/resources/run-liquibase.sh
+++ bahmni-emr/resources/run-liquibase.sh
@@ -2,5 +2,7 @@
 set -e -x
 
 . /etc/openmrs/bahmni-emr.conf
+. /etc/bahmni-installer/bahmni.conf
+
 
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$COMMON_CLASSPATH --changeLogFile=$1 --url=jdbc:mysql://$OPENMRS_DB_SERVER:3306/openmrs --username=$OPENMRS_DB_USERNAME --password=$OPENMRS_DB_PASSWORD update >> /bahmni_temp/logs/bahmni_deploy.log 2>> /bahmni_temp/logs/bahmni_deploy.log
--- openmrs/resources/initDB.sh
+++ openmrs/resources/initDB.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e -x
 
-RESULT=`mysql -uroot -ppassword --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
+. /etc/openmrs/openmrs.conf
+
+. /etc/bahmni-installer/bahmni.conf
+
+RESULT=`mysql -h $OPENMRS_DB_SERVER -uroot -ppassword --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
 if [ "$RESULT" != "openmrs" ]; then
     echo "openmrs database not found... Restoring a base dump"
-    mysql -uroot -ppassword openmrs < scripts/mrs_base.sql
-    mysql -uroot -ppassword -e "CREATE USER 'openmrs-user'@'localhost' IDENTIFIED BY '*';
-    GRANT ALL PRIVILEGES ON openmrs.* TO 'openmrs-user'@'localhost' identified by 'password'  WITH GRANT OPTION;"
+    mysql -h $OPENMRS_DB_SERVER -uroot -ppassword  < scripts/mrs_base.sql
+    mysql -h $OPENMRS_DB_SERVER -uroot -ppassword -e "CREATE USER 'openmrs-user'@'$OPENMRS_DB_SERVER' IDENTIFIED BY '*';
+    GRANT ALL PRIVILEGES ON openmrs.* TO 'openmrs-user'@'$OPENMRS_DB_SERVER' identified by 'password'  WITH GRANT OPTION;"
 fi
