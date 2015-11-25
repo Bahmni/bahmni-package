f7500b393509940902a7fd8b1191c3b94c795df6
rnjn <runjan@gmail.com>
2015-11-25 17:22:53 +0530

    rnjn| making openerp connect database configuration read common config

--- bahmni-erp-connect/resources/run-liquibase.sh
+++ bahmni-erp-connect/resources/run-liquibase.sh
@@ -3,10 +3,12 @@ set -e -x
 
 . /etc/bahmni-erp-connect/bahmni-erp-connect.conf
 
+. /etc/bahmni-installer/bahmni.conf
+
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock"
 LIQUIBASE_JAR="/opt/bahmni-erp-connect/bahmni-erp-connect/WEB-INF/lib/liquibase-core-2.0.3.jar"
 DRIVER="org.postgresql.Driver"
-CREDS="--url=jdbc:postgresql://$DB_SERVER:5432/openerp --username=$DB_USERNAME --password=$DB_PASSWORD"
+CREDS="--url=jdbc:postgresql://$OPENERP_DB_SERVER:5432/openerp --username=$OPENERP_DB_USERNAME --password=$OPENERP_DB_PASSWORD"
 CLASSPATH="/opt/openmrs/openmrs.war"
 CHANGE_LOG_FILE="/opt/bahmni-erp-connect/bahmni-erp-connect/WEB-INF/classes/sql/db_migrations.xml"
 
