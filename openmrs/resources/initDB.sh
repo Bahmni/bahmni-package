7c186b061cf0649d6501636fd485396cd852fb40
Preethi <preethi.ms34@gmail.com>
2015-11-25 12:01:34 +0530

    Preethi | Templatizing openmrs user &  pwd, mysql user& pwd in initDB.sh

--- openmrs/resources/initDB.sh
+++ openmrs/resources/initDB.sh
@@ -5,10 +5,10 @@ set -e -x
 
 . /etc/bahmni-installer/bahmni.conf
 
-RESULT=`mysql -h $OPENMRS_DB_SERVER -uroot -ppassword --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
+RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
 if [ "$RESULT" != "openmrs" ]; then
     echo "openmrs database not found... Restoring a base dump"
-    mysql -h $OPENMRS_DB_SERVER -uroot -ppassword  < scripts/mrs_base.sql
-    mysql -h $OPENMRS_DB_SERVER -uroot -ppassword -e "CREATE USER 'openmrs-user'@'$OPENMRS_DB_SERVER' IDENTIFIED BY '*';
-    GRANT ALL PRIVILEGES ON openmrs.* TO 'openmrs-user'@'$OPENMRS_DB_SERVER' identified by 'password'  WITH GRANT OPTION;"
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD  < scripts/mrs_base.sql
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$OPENMRS_DB_USER'@'$OPENMRS_DB_SERVER' IDENTIFIED BY '*';
+    GRANT ALL PRIVILEGES ON openmrs.* TO '$OPENMRS_DB_USER'@'$OPENMRS_DB_SERVER' identified by '$OPENMRS_DB_PASSWORD'  WITH GRANT OPTION;"
 fi
