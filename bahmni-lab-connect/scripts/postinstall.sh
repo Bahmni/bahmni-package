f29a60bbcfa4a01454da6a1eb3782eba26a90133
hemanths <hemanths@Administrators-MacBook-Pro.local>
2015-12-04 11:43:42 +0530

    Hemanth | Changing openmrs modules location to create symlink to elis atomfeed.

--- bahmni-lab-connect/scripts/postinstall.sh
+++ bahmni-lab-connect/scripts/postinstall.sh
@@ -1,7 +1,7 @@
 
 link_dirs(){
     mkdir -p /home/bahmni/.OpenMRS/modules
-    ln -s /opt/bahmni-lab-connect/modules/* /home/bahmni/.OpenMRS/modules
+    ln -s /opt/bahmni-lab-connect/modules/* /opt/openmrs/modules
     chown -R bahmni:bahmni /opt/bahmni-lab-connect/modules/
 }
 
