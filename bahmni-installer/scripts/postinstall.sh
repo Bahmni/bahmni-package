12b6cc0e7a82eeffcb51013d46d7718d14cc3995
Preethi <preethi.ms34@gmail.com>
2015-12-06 01:45:20 +0530

    Preethi | Fixing bahmni-installer rpm postinstall script

--- bahmni-installer/scripts/postinstall.sh
+++ bahmni-installer/scripts/postinstall.sh
@@ -2,4 +2,5 @@
 
 rm -f /usr/bin/bahmni
 ln -s /opt/bahmni-installer/bin/bahmni /usr/bin/bahmni
+rm -f /etc/bahmni-installer/rpm_versions.yml
 ln -s /opt/bahmni-installer/etc/rpm_versions.yml /etc/bahmni-installer/rpm_versions.yml
