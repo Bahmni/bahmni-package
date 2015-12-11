9b6a908ff0586ec41313cd9f7e942478fe283c0e
Preethi <preethi.ms34@gmail.com>
2015-12-11 15:51:03 +0530

    Preethi | Fixed owner of lab connect omod

--- bahmni-lab-connect/scripts/postinstall.sh
+++ bahmni-lab-connect/scripts/postinstall.sh
@@ -3,7 +3,8 @@ link_dirs(){
     rm -f /opt/openmrs/modules/openelis-atomfeed-client*.omod
     ln -s /opt/bahmni-lab-connect/modules/* /opt/openmrs/modules
     chown -R bahmni:bahmni /opt/bahmni-lab-connect/modules/
-    chown bahmni:bahmni /opt/openmrs/modules/openelis-atomfeed-client*.omod
+    chown -R bahmni:bahmni /opt/openmrs/modules/openelis-atomfeed-client*.omod
+    chmod 755 /opt/openmrs/modules/openelis-atomfeed-client*.omod
 }
 
 link_dirs
