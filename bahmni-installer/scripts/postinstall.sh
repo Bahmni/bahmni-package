6ffd89f50b2fff287967c45ac4110934a1985828
Preethi <preethi.ms34@gmail.com>
2016-01-12 00:08:40 +0530

    Preethi | Added log folder for bahmni-installer

--- bahmni-installer/scripts/postinstall.sh
+++ bahmni-installer/scripts/postinstall.sh
@@ -7,3 +7,5 @@ chown -R bahmni:bahmni /opt/bahmni-installer
 ln -s /opt/bahmni-installer/bin/bahmni /usr/bin/bahmni
 rm -f /etc/bahmni-installer/rpm_versions.yml
 ln -s /opt/bahmni-installer/etc/rpm_versions.yml /etc/bahmni-installer/rpm_versions.yml
+mkdir -p /var/log/bahmni-installer
+chown -R bahmni:bahmni /var/log/bahmni-installer
