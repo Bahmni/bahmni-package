bbbc1056e4f88a58a83967d50b4e4bd3dc924f38
Preethi <preethi.ms34@gmail.com>
2016-01-20 21:30:26 +0530

    #592 | Restored clean database when it is not default implementation and no database found

--- openmrs/resources/initDB.sh
+++ openmrs/resources/initDB.sh
@@ -8,9 +8,14 @@ if [ -f /etc/bahmni-installer/bahmni.conf ]; then
 fi
 
 RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'openmrs'"`
-if [ "$RESULT" != "openmrs" ] && [ "${IMPLEMENTATION_NAME:-default}" = "default" ]; then
+if [ "$RESULT" != "openmrs" ] ; then
+  if [ "${IMPLEMENTATION_NAME:-something}" = "default" ]; then
     echo "openmrs database not found... Restoring a base dump suitable to work with default config"
     mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD  < scripts/mrs_base.sql
+  else
+    echo "clean openmrs database will be created with no demo data"
+    mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD  < scripts/openmrs_clean_dump.sql
+  fi
 fi
 
 mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$OPENMRS_DB_USER'@'$OPENMRS_DB_SERVER' IDENTIFIED BY '*';
