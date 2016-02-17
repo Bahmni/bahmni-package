4589a58144700c9f1a1e453968b820d09d02f627
Preethi <preethi.ms34@gmail.com>
2016-02-17 22:50:54 +0530

    Preethi | Remove bahmni-playbooks from /opt during preinstall so that symlink created using vagrant-link.sh is removed

--- bahmni-installer/scripts/preinstall.sh
+++ bahmni-installer/scripts/preinstall.sh
@@ -1,3 +1,4 @@
 #create bahmni user and group if doesn't exist
 [ $(getent group bahmni) ]|| groupadd bahmni
 [ $(getent passwd bahmni) ] || useradd -g bahmni bahmni
+rm -rf /opt/bahmni-installer/bahmni-playbooks
