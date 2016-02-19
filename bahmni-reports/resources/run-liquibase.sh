6b7996ae59a9550bcfa60a90665857a3f9fd92fe
bharatak <bharatak@thoughtworks.com>
2016-02-19 11:00:37 +0530

    Bharat| Fixed Liquibase migration script issue

--- bahmni-reports/resources/run-liquibase.sh
+++ bahmni-reports/resources/run-liquibase.sh
@@ -9,7 +9,7 @@ fi
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=openmrs"
 LIQUIBASE_JAR="/opt/openmrs/openmrs/WEB-INF/lib/liquibase-core-2.0.5.jar"
 DRIVER="com.mysql.jdbc.Driver"
-CLASSPATH="/opt/bahmni-reports/bahmnireports.war"
+CLASSPATH="/opt/bahmni-reports/bahmni-reports/WEB-INF/lib/mysql-connector-java-5.1.6.jar"
 CHANGE_LOG_FILE="/opt/bahmni-reports/bahmni-reports/WEB-INF/classes/liquibase.xml"
 
 java $CHANGE_LOG_TABLE  -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --changeLogFile=$CHANGE_LOG_FILE --url=jdbc:mysql://$OPENMRS_DB_SERVER:3306/openmrs --username=$OPENMRS_DB_USERNAME --password=$OPENMRS_DB_PASSWORD update
