2eead0d02171c93b0ff73a6df7affe6226122ce8
vikashg <vikashg@INvikashg-2.local>
2016-03-01 15:08:55 +0530

    Vikash, Preethi | externalise OPENELIS_DB_SERVER for multi-machine setup.

--- bahmni-lab/resources/migrateDb.sh
+++ bahmni-lab/resources/migrateDb.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e -x
 
-java -jar -Dfile.encoding=utf-8 lib/liquibase-1.9.5.jar --defaultsFile=liquibase.properties --contexts=bahmni --url=jdbc:postgresql://localhost:5432/clinlims update
+. /etc/bahmni-lab/bahmni-lab.conf
+if [ -f /etc/bahmni-installer/bahmni.conf ]; then
+. /etc/bahmni-installer/bahmni.conf
+fi
+
+java -jar -Dfile.encoding=utf-8 lib/liquibase-1.9.5.jar --defaultsFile=liquibase.properties --contexts=bahmni --url=jdbc:postgresql://$OPENELIS_DB_SERVER:5432/clinlims update
