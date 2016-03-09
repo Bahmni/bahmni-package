35a43e11fa088fc8ff31adb5f7cca9426b828fca
vikashg <vikashg@INvikashg-2.local>
2016-03-09 15:33:46 +0530

    Vikash, Preethi | Migration should run only from active app server.

--- bahmni-lab/scripts/postinstall.sh
+++ bahmni-lab/scripts/postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f /etc/bahmni-installer/bahmni.conf ]; then
+. /etc/bahmni-installer/bahmni.conf
+fi
+
 #create bahmni user and group if doesn't exist
 USERID=bahmni
 GROUPID=bahmni
@@ -22,10 +26,10 @@ ln -s /opt/bahmni-lab/bahmni-lab /var/run/bahmni-lab/bahmni-lab
 ln -s /opt/bahmni-lab/log /var/log/bahmni-lab
 ln -s /opt/bahmni-lab/uploaded-files/elis /home/bahmni/uploaded-files/elis
 
-#create a database if it doesn't exist.
-if [ -z "$IS_PASSIVE" && $IS_PASSIVE -ne 1 ]; then
-(cd /opt/bahmni-lab/migrations && scripts/initDB.sh bahmni-base.dump)
-(cd /opt/bahmni-lab/migrations/liquibase/ && /opt/bahmni-lab/migrations/scripts/migrateDb.sh)
+#create a database if it doesn't exist and if it is not passive machine.
+if [ "${IS_PASSIVE:-0}" -ne "1" ]; then
+    (cd /opt/bahmni-lab/migrations && scripts/initDB.sh bahmni-base.dump)
+    (cd /opt/bahmni-lab/migrations/liquibase/ && /opt/bahmni-lab/migrations/scripts/migrateDb.sh)
 fi
 
 cp -f /opt/bahmni-lab/etc/atomfeed.properties /opt/bahmni-lab/bahmni-lab/WEB-INF/classes/atomfeed.properties
