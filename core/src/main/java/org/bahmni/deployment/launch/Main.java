43810f01205b36a1dc6530bf31ab7af2486462a2
bharatak <bharatak@thoughtworks.com>
2016-03-11 11:30:44 +0530

    Bharat| #0000 | Setting the Tomcat session timeout to 120 minutes

--- core/src/main/java/org/bahmni/deployment/launch/Main.java
+++ core/src/main/java/org/bahmni/deployment/launch/Main.java
@@ -18,6 +18,7 @@ public class Main {
 		tomcat.setPort(valueOf(getenv("SERVER_PORT")));
 		tomcat.setBaseDir(getenv("BASE_DIR"));
 		Context context = tomcat.addWebapp(getenv("CONTEXT_PATH"), new File(getenv("WAR_DIRECTORY")).getAbsolutePath());
+		context.setSessionTimeout(120);
 		context.setUseHttpOnly(false);
 		tomcat.start();
 		tomcat.getServer().await();
