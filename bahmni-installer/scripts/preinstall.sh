fa185e3416f313e30552af248d7b92b67b0e111b
Dé <rahul080327@gmail.com>
2016-03-18 17:54:28 +0530

    Rahul | Create branch for older sh based installer, till py installers are fixed

--- bahmni-installer/scripts/preinstall.sh
+++ bahmni-installer/scripts/preinstall.sh
@@ -4,12 +4,3 @@
 [ $(getent group bahmni) ]|| groupadd bahmni
 [ $(getent passwd bahmni) ] || useradd -g bahmni bahmni
 rm -rf /opt/bahmni-installer/bahmni-playbooks
-
-if [[ ! -f /opt/get-pip.py ]]; then
-  echo " Downloading get-pip.py "
-  curl https://bootstrap.pypa.io/get-pip.py -o "/opt/get-pip.py"
-  echo "Download complete"
-fi
-echo " Installing pip "
-python /opt/get-pip.py
-pip install -U setuptools
