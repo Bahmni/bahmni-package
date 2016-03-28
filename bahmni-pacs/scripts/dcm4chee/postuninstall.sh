cd77742b52bc42c37cbdaed6f6b1c1c3e1399dfa
Preethi <preethi.ms34@gmail.com>
2016-03-28 13:24:21 +0530

    More changes with dcm4chee rpm

--- bahmni-pacs/scripts/dcm4chee/postuninstall.sh
+++ bahmni-pacs/scripts/dcm4chee/postuninstall.sh
@@ -2,5 +2,6 @@
 
 rm -rf /etc/init.d/dcm4chee
 rm -rf /opt/dcm4chee
+rm -rf /etc/dcm4chee
 rm -rf /var/lib/bahmni/dcm4chee-2.18.1-psql
 rm -rf /usr/share/jboss-4.2.3.GA
--- bahmni-pacs/test/download-artifacts.sh
+++ bahmni-pacs/test/download-artifacts.sh
@@ -2,4 +2,4 @@
 
 . ../../test-common/test.conf
 
-../../test-common/download-artifact.sh /go/files/Bahmni_pacs_$BRANCH/Latest/Build/Latest/build/deployables/pacs-integration.war ../resources/pacs-integration.war
+../../test-common/download-artifact.sh /go/files/Bahmni_pacs_$BRANCH/Latest/Build/Latest/build/deployables/pacs-integration.war ../resources/pacs-integration/pacs-integration.war
