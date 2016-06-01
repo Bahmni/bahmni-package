0dc40dd6b44acb378c3736b043af1abb677b3e8d
Arjun <arjunk@thoughtworks.com>
2016-06-01 10:57:22 +0530

    #1786 | Pankaj, Arjun | Parametrising the database server value for dcm4chee so that db and service can be deployed on separate machines.

--- bahmni-pacs/resources/dcm4chee/initDCM4CHEE-DB.sh
+++ bahmni-pacs/resources/dcm4chee/initDCM4CHEE-DB.sh
@@ -6,9 +6,9 @@ if [ -f /etc/bahmni-installer/bahmni.conf ]; then
 . /etc/bahmni-installer/bahmni.conf
 fi
 
-if [ "$(psql -U$POSTGRES_USER -lqt | cut -d \| -f 1 | grep -w $PACS_DB | wc -l)" -eq 0 ]
+if [ "$(psql -h$DCM4CHEE_DB_SERVER -U$POSTGRES_USER -lqt | cut -d \| -f 1 | grep -w $PACS_DB | wc -l)" -eq 0 ]
 then
     export PGUSER=$POSTGRES_USER
-    psql -U$POSTGRES_USER -c "CREATE DATABASE $PACS_DB;"
-    psql -U$POSTGRES_USER $PACS_DB -f /var/lib/bahmni/dcm4chee-2.18.1-psql/sql/create.psql
+    psql -h$DCM4CHEE_DB_SERVER -U$POSTGRES_USER -c "CREATE DATABASE $PACS_DB;"
+    psql -h$DCM4CHEE_DB_SERVER -U$POSTGRES_USER $PACS_DB -f /var/lib/bahmni/dcm4chee-2.18.1-psql/sql/create.psql
 fi
