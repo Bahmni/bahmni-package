b66eff0508a11d2b414c69f3e2b2c48b03996f8e
chethandeshpande <chethandec22@gmail.com>
2016-06-07 12:29:20 +0530

    Chethan, Arjun | Fixing issues with dcm4chee rpm.
    1. Fixed issue of archive directory getting deleted during installation.
    2. Fixed issue of unwanted jars getting installed with dcm4chee which was causing conflicts.

--- bahmni-pacs/scripts/dcm4chee/preuninstall.sh
+++ bahmni-pacs/scripts/dcm4chee/preuninstall.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 service dcm4chee stop
-
-if test -d /var/lib/bahmni/dcm4chee-2.18.1-psql/server/default/archive;
-then
-    sudo mv /var/lib/bahmni/dcm4chee-2.18.1-psql/server/default/archive /var/lib/bahmni/
-fi
