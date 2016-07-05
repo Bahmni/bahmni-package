8e6851e6c7c2e5407929b020af81404212894e3f
senthilr <senthilr@thoughtworks.com>
2016-07-05 13:24:28 +0530

    introduced bahmni version in index page

--- bahmni-web/test/bahmni_openmrs_revision.sh
+++ bahmni-web/test/bahmni_openmrs_revision.sh
@@ -7,7 +7,8 @@ revision='{
         "openmrs_distro_bahmni" : "https://github.com/Bahmni/openmrs-distro-bahmni/commit/_openmrsSha_",
         "bahmni_app" : "https://github.com/Bahmni/openmrs-module-bahmniapps/commit/_openmrsModulesSha_",
         "functional_tests" : "https://github.com/Bahmni/emr-functional-tests/commit/_functionalTestsSha_",
-        "config" : "https://github.com/Bahmni/default-config/commit/_defaultConfigSha_"
+        "config" : "https://github.com/Bahmni/default-config/commit/_defaultConfigSha_",
+        "Versions" "/versions"
     }
 }'
 
