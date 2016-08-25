ab99ddeb6d636067dbf510d5300b897b41d41881
binduak <bindu.akkin@gmail.com>
2016-08-25 11:14:24 +0530

    bindu | #2074 | Deleted Source Language file(locale_en.json)

--- bahmni-transifex/scripts/postinstall.sh
+++ bahmni-transifex/scripts/postinstall.sh
@@ -4,16 +4,5 @@ TRANSIFEX_PATH="/opt/bahmni-transifex/transifex"
 
 echo "running the postinstall script"
 
-
-#This is a temporary fix.  Will issue a Pull Request for migration of global_property <locale.allowed.list> for bahmni-core
-
-mv $TRANSIFEX_PATH/admin/locale_pt_BR.json $TRANSIFEX_PATH/admin/locale_pt.json
-mv $TRANSIFEX_PATH/adt/locale_pt_BR.json $TRANSIFEX_PATH/adt/locale_pt.json 
-mv $TRANSIFEX_PATH/clinical/locale_pt_BR.json $TRANSIFEX_PATH/clinical/locale_pt.json
-mv $TRANSIFEX_PATH/home/locale_pt_BR.json $TRANSIFEX_PATH/home/locale_pt.json
-mv $TRANSIFEX_PATH/orders/locale_pt_BR.json $TRANSIFEX_PATH/orders/locale_pt.json
-mv $TRANSIFEX_PATH/registration/locale_pt_BR.json $TRANSIFEX_PATH/registration/locale_pt.json
-mv $TRANSIFEX_PATH/reports/locale_pt_BR.json $TRANSIFEX_PATH/reports/locale_pt.json
-
 cp -rf $TRANSIFEX_PATH/* /var/www/bahmniapps/i18n/.
 
