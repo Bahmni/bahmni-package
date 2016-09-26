4f3b43131641af768198ed3fedba6de776ab1f80
Alaggesan Palani <alagesann@gmail.com>
2016-09-26 12:21:53 +0530

    Alagesan, Salauddin| #2469 | Introduce new variables for Bahmni reports db and openmrs variable refactoring

--- bahmni-reports/resources/initDB.sh
+++ bahmni-reports/resources/initDB.sh
@@ -6,8 +6,8 @@ if [ -f /etc/bahmni-installer/bahmni-emr-installer.conf ]; then
 . /etc/bahmni-installer/bahmni-emr-installer.conf
 fi
 
-RESULT=`mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'bahmni_reports'"`
+RESULT=`mysql -h $REPORTS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE 'bahmni_reports'"`
 if [ "$RESULT" != "bahmni_reports" ] ; then
-  mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE bahmni_reports;"
-  mysql -h $OPENMRS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bahmni_reports.* TO '$OPENMRS_DB_USERNAME'@'$OPENMRS_DB_SERVER' identified by '$OPENMRS_DB_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  mysql -h $REPORTS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE bahmni_reports;"
+  mysql -h $REPORTS_DB_SERVER -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON bahmni_reports.* TO '$REPORTS_DB_USERNAME'@'$REPORTS_DB_SERVER' identified by '$REPORTS_DB_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 fi
