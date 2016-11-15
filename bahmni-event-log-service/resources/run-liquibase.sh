8926b34b6bc35822a860f75a5cb37c95973d211b
hanishar <hanisha946@gmail.com>
2016-11-15 10:32:57 +0530

    Hanisha, ShruthiP | #2796 | 1.Change the run liquibase script of eventlog service to take openmrs conf 2. Make liquibase migration to use defualt change log table(databasechangelog)

--- bahmni-event-log-service/resources/run-liquibase.sh
+++ bahmni-event-log-service/resources/run-liquibase.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 set -e -x
 
-. /etc/bahmni-reports/bahmni-reports.conf
+. /etc/openmrs/openmrs.conf
 if [ -f /etc/bahmni-installer/bahmni.conf ]; then
 . /etc/bahmni-installer/bahmni.conf
 fi
 
-CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=openmrs"
 LIQUIBASE_JAR="/opt/openmrs/openmrs/WEB-INF/lib/liquibase-core-2.0.5.jar"
 DRIVER="com.mysql.jdbc.Driver"
-CLASSPATH="/opt/bahmni-event-log-service/event-log-service-webapp.war"
+CLASSPATH="/opt/bahmni-event-log-service/bahmni-event-log-service/WEB-INF/lib/mysql-connector-java-5.1.38.jar"
 CHANGE_LOG_FILE="/opt/bahmni-event-log-service/bahmni-event-log-service/WEB-INF/classes/db/changelog/liquibase.xml"
 
-java $CHANGE_LOG_TABLE  -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --changeLogFile=$CHANGE_LOG_FILE --url=jdbc:mysql://$OPENMRS_DB_SERVER:3306/openmrs --username=$OPENMRS_DB_USERNAME --password=$OPENMRS_DB_PASSWORD update
+java  -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --changeLogFile=$CHANGE_LOG_FILE --url=jdbc:mysql://$OPENMRS_DB_SERVER:3306/openmrs --username=$OPENMRS_DB_USERNAME --password=$OPENMRS_DB_PASSWORD update
 
 
