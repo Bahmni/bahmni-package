f52bf5d99995942548c2158a3f621bd38d117454
salauddin <snalband@thoughtworks.com>
2016-12-09 11:40:44 +0530

    Salauddin|#2842|Fixed typo

--- bahmni-installer/resources/bahmni-command-line-tool/bahmni.py
+++ bahmni-installer/resources/bahmni-command-line-tool/bahmni.py
@@ -47,7 +47,7 @@ def cli(ctx, implementation, inventory, sql_path, database, verbose, implementat
 def installAnsible(ansible_rpm_url):
     print "Installing Ansible..."
     ansible_installation_process = subprocess.Popen(['sudo yum install -y ' + ansible_rpm_url], stdout=subprocess.PIPE,
-                                                    stderrg=subprocess.PIPE, shell=True)
+                                                    stderr=subprocess.PIPE, shell=True)
     output, err = ansible_installation_process.communicate()
     print output, err
     if ansible_installation_process.returncode != 0:
