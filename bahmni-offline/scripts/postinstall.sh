fd80304d247b56d05a696cd9bdf70e827f13a1c7
hanishar <hanisha946@gmail.com>
2017-03-03 12:18:47 +0530

    Hanisha | 3307 | Packaging bahmni connect apps in bahmni-offline rpm

---
+++ bahmni-offline/scripts/postinstall.sh
@@ -0,0 +1,3 @@
+ln -s /opt/bahmni-offline/bahmni-connect-apps/ /var/www/bahmni-connect-apps
+
+chown -R bahmni:bahmni /var/www/bahmni-connect-apps
--- bahmni-offline/scripts/preinstall.sh
+++ bahmni-offline/scripts/preinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo rm -rf /opt/openmrs/modules/bahmniOfflineSync-*.omod
+sudo rm -f /var/www/bahmni-connect-apps
+
