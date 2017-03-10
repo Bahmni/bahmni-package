94d21954cd5608f488e9886675636cb1c6afdc06
hanishar <hanisha946@gmail.com>
2017-03-10 17:29:31 +0530

    Hanisha | Gave relevant permission to bahmni-connect-apps in /var/www

--- bahmni-offline/scripts/postinstall.sh
+++ bahmni-offline/scripts/postinstall.sh
@@ -1,3 +1,10 @@
 ln -s /opt/bahmni-offline/bahmni-connect-apps/ /var/www/bahmni-connect-apps
 
+
+chown -R bahmni:bahmni /opt/bahmni-offline
+
+chmod 755 /opt/bahmni-offline
+
 chown -R bahmni:bahmni /var/www/bahmni-connect-apps
+
+chmod -R 755 /var/www/bahmni-connect-apps
