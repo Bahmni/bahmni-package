c59841d4aec1d004a70b666031d941f7bae9f9d3
shilpa-iplit <sparab@iplit.in>
2018-11-23 15:19:44 +0530

    Shilpa|BAH-631|Bahmni-package merging of bahmni-openmrs into bahmni-emr

--- bahmni-emr/scripts/postinstall.sh
+++ bahmni-emr/scripts/postinstall.sh
@@ -8,6 +8,25 @@ if [ -f /etc/bahmni-installer/bahmni.conf ]; then
 . /etc/bahmni-installer/bahmni.conf
 fi
 
+#create links
+ln -s /opt/openmrs/etc /etc/openmrs
+ln -s /opt/openmrs/bin/openmrs /etc/init.d/openmrs
+ln -s /opt/openmrs/run /var/run/openmrs
+ln -s /opt/openmrs/openmrs /var/run/openmrs/openmrs
+ln -s /opt/openmrs/log /var/log/openmrs
+
+(cd /opt/openmrs/openmrs && unzip ../openmrs.war)
+# restore mrs db dump if database doesn't exists
+
+if [ "${IS_PASSIVE:-0}" -ne "1" ]; then
+    (cd /opt/openmrs/openmrs && scripts/initDB.sh)
+fi
+chkconfig --add openmrs
+
+#copy configs
+mkdir -p /opt/openmrs/openmrs/WEB-INF/classes/ && cp /opt/openmrs/etc/log4j.xml /opt/openmrs/openmrs/WEB-INF/classes/
+cp -f /opt/openmrs/etc/web.xml /opt/openmrs/openmrs/WEB-INF/
+
 link_dirs(){
     rm -rf /home/$OPENMRS_SERVER_USER/.OpenMRS/modules
     ln -s $MODULE_REPO /home/$OPENMRS_SERVER_USER/.OpenMRS/modules
@@ -56,6 +75,3 @@ if [ "${IS_PASSIVE:-0}" -ne "1" ]; then
 fi
 create_configuration_dirs
 setupConfFiles
-
-
-
