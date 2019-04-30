e78aed056ab52c2e5099f409769cad8b23854409
tbindu <tbindu@thoughtworks.com>
2019-04-30 11:41:53 +0530

    Bindu, Angshu | BAH-749 | Changing openerp db name to odoo

--- bahmni-erp-connect/resources/run-liquibase.sh
+++ bahmni-erp-connect/resources/run-liquibase.sh
@@ -10,7 +10,7 @@ fi
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock"
 LIQUIBASE_JAR="/opt/bahmni-erp-connect/bahmni-erp-connect/WEB-INF/lib/liquibase-core-2.0.3.jar"
 DRIVER="org.postgresql.Driver"
-CREDS="--url=jdbc:postgresql://$OPENERP_DB_SERVER:5432/openerp --username=$OPENERP_DB_USERNAME --password=$OPENERP_DB_PASSWORD"
+CREDS="--url=jdbc:postgresql://$OPENERP_DB_SERVER:5432/odoo --username=$OPENERP_DB_USERNAME --password=$OPENERP_DB_PASSWORD"
 CLASSPATH="/opt/openmrs/openmrs.war"
 CHANGE_LOG_FILE="/opt/bahmni-erp-connect/bahmni-erp-connect/WEB-INF/classes/sql/db_migrations.xml"
 
