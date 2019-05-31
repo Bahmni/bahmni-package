f9016c42f2f62188590a6750e7a97d224a526576
Rajashri <jadhavrajashri49@gmail.com>
2019-05-31 15:38:39 +0530

    Revert "Use clean demp for default implementtation, this commit to be reverted once new demo dump is updated"
    This reverts commit f6a2ef412cfaa8f258180e66f0754f9519d56989.

--- bahmni-erp/scripts/postinstall.sh
+++ bahmni-erp/scripts/postinstall.sh
@@ -52,9 +52,7 @@ initDB(){
         createdb -Uodoo -h$ODOO_DB_SERVER odoo;
 
          if [ "${IMPLEMENTATION_NAME:-default}" = "default" ]; then
-#           psql -Uodoo -h$ODOO_DB_SERVER odoo < /opt/bahmni-erp/db-dump/odoo_demo_dump.sql
-            psql -Uodoo -h$ODOO_DB_SERVER odoo < /opt/bahmni-erp/db-dump/odoo_clean_dump.sql
-
+            psql -Uodoo -h$ODOO_DB_SERVER odoo < /opt/bahmni-erp/db-dump/odoo_demo_dump.sql
         else
             psql -Uodoo -h$ODOO_DB_SERVER odoo < /opt/bahmni-erp/db-dump/odoo_clean_dump.sql
         fi
