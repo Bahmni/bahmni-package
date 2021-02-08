0a4815f64a52e4a9dce64e1b02615c62f00787e5
Buvaneswari Arun <buvaneswari.arun@thoughtworks.com>
2021-02-08 13:01:34 +0530

    BAH-1116|Buvaneswari|Click installation issue

--- bahmni-installer/scripts/preinstall.sh
+++ bahmni-installer/scripts/preinstall.sh
@@ -9,3 +9,6 @@ rm -rf /usr/bin/bahmni
 rm -rf /etc/bahmni-installer/deployment-artifacts/rpm_versions.yml
 rm -rf /etc/bahmni-installer/deployment-artifacts/local
 rm -rf /var/log/bahmni-installer
+pip install pip==v19.0
+pip uninstall click
+pip install click==v7.0
