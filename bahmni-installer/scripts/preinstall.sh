416d29380e046af03392a1238e63b17b4cb41bd8
buvaneswari-arun <buvaneswari.arun@thoughtworks.com>
2021-04-06 23:23:08 +0530

    Buvaneswari|CI failure fix

--- bahmni-installer/scripts/preinstall.sh
+++ bahmni-installer/scripts/preinstall.sh
@@ -10,5 +10,7 @@ rm -rf /etc/bahmni-installer/deployment-artifacts/rpm_versions.yml
 rm -rf /etc/bahmni-installer/deployment-artifacts/local
 rm -rf /var/log/bahmni-installer
 pip install pip==v19.0
+pip install --upgrade setuptools
 pip uninstall click
 pip install click==v7.0
+
