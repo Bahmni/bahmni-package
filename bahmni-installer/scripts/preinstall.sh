39e972a92eb993b3873eab11f9d12bce612170bd
buvaneswari-arun <buvaneswari.arun@thoughtworks.com>
2021-04-08 20:00:34 +0530

    BAh-1187|Buvaneswari|Bahmni installation failed because of non availability of babel and decorator

--- bahmni-installer/scripts/preinstall.sh
+++ bahmni-installer/scripts/preinstall.sh
@@ -10,7 +10,8 @@ rm -rf /etc/bahmni-installer/deployment-artifacts/rpm_versions.yml
 rm -rf /etc/bahmni-installer/deployment-artifacts/local
 rm -rf /var/log/bahmni-installer
 pip install pip==v19.0
-pip install --upgrade setuptools
+pip install babel==v0.9.6
+pip install decorator==v3.4.0
 pip uninstall click
 pip install click==v7.0
 
