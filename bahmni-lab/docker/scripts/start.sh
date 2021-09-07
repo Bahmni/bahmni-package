509b85c28827b7f9ff68516acf1f7217d9b41abc
MOHANKUMAR T <mohan13081999@gmail.com>
2021-09-07 16:41:02 +0530

    BDI-4|Mohan|Refactor| Start script to run implementation specific migrations

--- bahmni-lab/docker/scripts/start.sh
+++ bahmni-lab/docker/scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+set -e
+echo "[INFO] Running Default Liquibase migrations"
 cd /opt/bahmni-lab/migrations/liquibase/ && sh /opt/bahmni-lab/migrations/scripts/migrateDb.sh
+echo "[INFO] Running User Defined Liquibase migrations"
+sh /opt/bahmni-lab/migrations/scripts/run-implementation-openelis-implementation.sh
+echo "[INFO] Starting Application"
 java -jar $SERVER_OPTS $DEBUG_OPTS /opt/bahmni-lab/lib/bahmni-lab.jar
