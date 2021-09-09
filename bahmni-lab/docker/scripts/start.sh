37024248e8aa302b016e228bb667a83a0bfe1d36
MOHANKUMAR T <mohan13081999@gmail.com>
2021-09-09 17:01:57 +0530

    BDI-4|Mohan|Add| Commands to replace host names in config files based on ENV variable

--- bahmni-lab/docker/scripts/start.sh
+++ bahmni-lab/docker/scripts/start.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 set -e
+
+replaceConfigFiles(){
+    #TO-DO: Look for a better way to use Environemnt variables in these config files.
+    ATOMFEED_PROPERTIES_FILE=${WAR_DIRECTORY}/WEB-INF/classes/atomfeed.properties
+    ATOMFEED_PROPERTIES_BACKUP_FILE=${WAR_DIRECTORY}/WEB-INF/classes/atomfeed.properties.backup
+    HIBERNATE_CONFIG_FILE=${WAR_DIRECTORY}/WEB-INF/classes/us/mn/state/health/lims/hibernate/hibernate.cfg.xml
+    HIBERNATE_CONFIG_BACKUP_FILE=${WAR_DIRECTORY}/WEB-INF/classes/us/mn/state/health/lims/hibernate/hibernate.cfg.xml.backup
+    if [ ! -f ${ATOMFEED_PROPERTIES_BACKUP_FILE} ]; then
+        cp ${ATOMFEED_PROPERTIES_FILE} ${ATOMFEED_PROPERTIES_BACKUP_FILE}
+    fi
+    if [ ! -f ${HIBERNATE_CONFIG_BACKUP_FILE} ]; then
+        cp ${HIBERNATE_CONFIG_FILE} ${HIBERNATE_CONFIG_BACKUP_FILE}
+    fi
+    sed "s/localhost/${OPENMRS_HOST}/" ${ATOMFEED_PROPERTIES_BACKUP_FILE} > ${ATOMFEED_PROPERTIES_FILE}
+    sed "s/localhost/${OPENELIS_DB_SERVER}/" ${HIBERNATE_CONFIG_BACKUP_FILE} > ${HIBERNATE_CONFIG_FILE}
+}
+
+replaceConfigFiles
 echo "[INFO] Running Default Liquibase migrations"
 cd /opt/bahmni-lab/migrations/liquibase/ && sh /opt/bahmni-lab/migrations/scripts/migrateDb.sh
 echo "[INFO] Running User Defined Liquibase migrations"
