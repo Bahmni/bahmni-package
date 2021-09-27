6f64adc0bd8cfd966c34d18e1fb0421ef28aaa92
MOHANKUMAR T <mohan13081999@gmail.com>
2021-09-27 09:15:08 +0530

    BDI-17|Mohan|Refactor| Rename GO_PIPELINE_COUNTER to GITHUB_RUN_NUMBER in Odoo Build scripts

--- bahmni-docker/build_scripts/bahmni-erp/docker_build.sh
+++ bahmni-docker/build_scripts/bahmni-erp/docker_build.sh
@@ -9,7 +9,7 @@ cd bahmni-package/bahmni-erp
 # Unzipping Odoo Modules copied by CI
 unzip -q -u -d build/ resources/odoo-modules.zip
 #Building Docker images
-ODOO_IMAGE_TAG=${BAHMNI_VERSION}-${GO_PIPELINE_COUNTER}
+ODOO_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
 docker build -t bahmni/odoo-10-db:fresh-${ODOO_IMAGE_TAG} -f docker/db.Dockerfile  . --no-cache
 docker build -t bahmni/odoo-10-db:demo-${ODOO_IMAGE_TAG} -f docker/demodb.Dockerfile  . --no-cache
 docker build -t bahmni/odoo-10:${ODOO_IMAGE_TAG} -f docker/Dockerfile  . --no-cache
