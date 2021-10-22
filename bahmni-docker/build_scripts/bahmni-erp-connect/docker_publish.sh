4e50f69a6af656a21d48a14a02dd74e43401443a
MOHANKUMAR T <mohan13081999@gmail.com>
2021-10-22 14:01:35 +0530

    BDI-35|Mohan|Refactor| Bahmni erp connect docker publish script to push SNAPSHOT images

--- bahmni-docker/build_scripts/bahmni-erp-connect/docker_publish.sh
+++ bahmni-docker/build_scripts/bahmni-erp-connect/docker_publish.sh
@@ -2,5 +2,14 @@
 set -e
 ODOO_CONNECT_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
 echo ${DOCKER_HUB_TOKEN} | docker login -u ${DOCKER_HUB_USERNAME} --password-stdin
+echo "[INFO] Pushing build images"
 docker push bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG}
+
+echo "[INFO] Tagging build images as SNAPSHOT Images"
+ODOO_CONNECT_SNAPSHOT_IMAGE_TAG=${BAHMNI_VERSION}-SNAPSHOT
+docker tag bahmni/odoo-connect:${ODOO_CONNECT_IMAGE_TAG} bahmni/odoo-connect:${ODOO_CONNECT_SNAPSHOT_IMAGE_TAG}
+
+echo "[INFO] Pushing SNAPSHOT images"
+docker push bahmni/odoo-connect:${ODOO_CONNECT_SNAPSHOT_IMAGE_TAG}
+
 docker logout
