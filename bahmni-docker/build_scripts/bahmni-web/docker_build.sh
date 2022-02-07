53c2a8c6fefe961b9b4328301b5026cbf285e680
swatigogia2020 <swati.gogia@thoughtworks.com>
2022-02-07 12:34:00 +0530

    BAH-1381: changing the build acript for web to pickup default_config artifact

--- bahmni-docker/build_scripts/bahmni-web/docker_build.sh
+++ bahmni-docker/build_scripts/bahmni-web/docker_build.sh
@@ -2,8 +2,7 @@
 set -xe
 
 #Working directory : default-config
-cd default-config && scripts/package.sh && cd ..
-cp default-config/target/default_config.zip bahmni-package/bahmni-web/resources/
+cp default_config.zip bahmni-package/bahmni-web/resources/
 
 cd bahmni-package/bahmni-web
 rm -rf build/
