becdeff64d9a05d529460a3cbd9ba3ee152a21ab
MOHANKUMAR T <mohan13081999@gmail.com>
2022-07-26 19:09:56 +0530

    BAH-2054 | Refactor dcm4chee database initialisation
    Co-authored-by: Divij Goyal <divij.g@beehyv.com>

--- bahmni-pacs/docker/dcm4chee/scripts/startup.sh
+++ bahmni-pacs/docker/dcm4chee/scripts/startup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 set -e
+initialise_database() {
+    export PGPASSWORD="${DB_PASSWORD}"
+    RESULT=$(psql "${DB_NAME}" --host "${DB_HOST}" --port "${DB_PORT}" -U "${DB_USERNAME}" -A -t -c "SELECT count(*) FROM pg_tables WHERE schemaname='public';")
+    if [ "$RESULT" = "0" ]
+    then
+      echo "[INFO] Initialising tables in "${DB_NAME}" database.."
+      PGPASSWORD="${DB_PASSWORD}" psql "${DB_NAME}" --host "${DB_HOST}" --port "${DB_PORT}" -U "${DB_USERNAME}" -f "${DCM4CHEE_PATH}"/sql/create.psql
+    else
+      echo "[INFO] Database already initialised."
+    fi
+}
 
 rm "${DCM4CHEE_PATH}"/server/default/deploy/pacs-postgres-ds.xml
 envsubst < /etc/pacs-postgres-ds.xml.template > "${DCM4CHEE_PATH}"/server/default/deploy/pacs-postgres-ds.xml
@@ -7,6 +18,5 @@ envsubst < /etc/pacs-postgres-ds.xml.template > "${DCM4CHEE_PATH}"/server/defaul
 echo "Waiting for ${DB_HOST}.."
 sh wait-for.sh -t 300 "${DB_HOST}":"${DB_PORT}"
 
-PGPASSWORD="${DB_PASSWORD}" psql "${DB_NAME}" --host "${DB_HOST}" -U "${DB_USERNAME}" -f "${DCM4CHEE_PATH}"/sql/create.psql
-
+initialise_database
 "${DCM4CHEE_PATH}"/bin/run.sh
