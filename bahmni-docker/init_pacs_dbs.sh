a3243e99941c2e94a7dc3b2afb403ac139e4a319
MOHANKUMAR T <mohan13081999@gmail.com>
2022-07-27 16:16:02 +0530

    BAH-1422 | Refactor Use single postgres server for dcm4chee, pacs-integration
    Co-authored-by: Divij Goyal <divij.g@beehyv.com>

---
+++ bahmni-docker/init_pacs_dbs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+create_user_and_database() {
+  export PG_PASSWORD=${POSTGRES_PASSWORD}
+  DB_NAME="$1"
+  DB_USERNAME="$2"
+  DB_PASSWORD="$3"
+  echo "$3"
+  psql -c "CREATE DATABASE ${DB_NAME};"
+  psql -c "REVOKE ALL ON DATABASE ${DB_NAME} FROM PUBLIC;"
+  psql -c "CREATE USER ${DB_USERNAME} WITH ENCRYPTED PASSWORD '${DB_PASSWORD}';"
+  psql -c "GRANT ALL ON DATABASE ${DB_NAME} TO ${DB_USERNAME};"
+}
+
+create_user_and_database "${PACS_DB_NAME}" "${PACS_DB_USERNAME}" "${PACS_DB_PASSWORD}"
+create_user_and_database "${PACS_INTEGRATION_DB_NAME}" "${PACS_INTEGRATION_DB_USERNAME}" "${PACS_INTEGRATION_DB_PASSWORD}"
