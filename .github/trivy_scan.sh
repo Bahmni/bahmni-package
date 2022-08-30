d8a9fffb646075b6e2bf66f0a8a90ca39e74b917
Umair Fayaz <omayrfayaz@gmail.com>
2022-08-30 16:36:40 +0530

    BAH-2193 | Refactored script
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>

--- .github/trivy_scan.sh
+++ .github/trivy_scan.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+PATH_TO_DIRECTORY=$1
+PATH_TO_WORKFLOW=$2
 #Install Trivy Scanner
 brew install aquasecurity/trivy/trivy
 
-#Run Trivy Scan
-trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 .
+if [[ -z "$PATH_TO_WORKFLOW"  ]] ; then
+   # if PATH_TO_WORKFLOW not provided, scan PATH_TO_DIRECTORY
+   trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $PATH_TO_DIRECTORY
+elif [[ -z "$PATH_TO_DIRECTORY" && -z "$PATH_TO_WORKFLOW" ]] ; then
+    # if PATH_TO_WORKFLOW & PATH_TO_DIRECTORY not provided, scan the whole filesystem
+    trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 .
+else
+    trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $PATH_TO_DIRECTORY
+    trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $PATH_TO_WORKFLOW
+
+fi
+
+
