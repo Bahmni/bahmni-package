c4a89ed299459b37da4b5b8582d27339fcb11b35
stavtnameh <hemanth.v@beehyv.com>
2022-08-30 17:33:36 +0530

    BAH-2193| changed trivy_scan bash script to consider the special paths
    Co-authored-by: Umair Fayaz <omayrfayaz@gmail.com>

--- .github/trivy_scan.sh
+++ .github/trivy_scan.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-PATH_TO_DIRECTORY=$1
-PATH_TO_WORKFLOW=$2
 #Install Trivy Scanner
 brew install aquasecurity/trivy/trivy
-
-if [[ -z "$PATH_TO_WORKFLOW"  ]] ; then
-   # if PATH_TO_WORKFLOW not provided, scan PATH_TO_DIRECTORY
-   trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $PATH_TO_DIRECTORY
-elif [[ -z "$PATH_TO_DIRECTORY" && -z "$PATH_TO_WORKFLOW" ]] ; then
-    # if PATH_TO_WORKFLOW & PATH_TO_DIRECTORY not provided, scan the whole filesystem
-    trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 .
-else
-    trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $PATH_TO_DIRECTORY
-    trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $PATH_TO_WORKFLOW
-
+#Make an array of input paths
+TARGETS=("$@")
+#Count number of inputs
+NUMBER_OF_PATHS=${#args[@]} 
+#If no input given scan all
+if [ -z "$TARGETS" ]; then
+    TARGETS=(".")
 fi
-
-
+#Trivy scan the elements of array
+for TARGET_PATH in "${TARGETS[@]}";
+do
+echo "scanning $TARGET_PATH"
+trivy fs --severity "MEDIUM,HIGH,CRITICAL" --exit-code 1 $TARGET_PATH
+done
