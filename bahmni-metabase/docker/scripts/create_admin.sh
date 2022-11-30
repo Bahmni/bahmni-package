8adb4120a6e4a7a8f5c1770a807feeec743d5839
sindhura-kaparthi <sindhura.kaparthi@thoughtworks.com>
2022-11-30 18:09:06 +0530

    Add. Scripts and Dockerfile for building bahmni metabase

--- bahmni-metabase/docker/scripts/create_admin.sh
+++ bahmni-metabase/docker/scripts/create_admin.sh
@@ -4,26 +4,25 @@ echo "Creating admin user"
 
 SETUP_TOKEN=$(curl -s -m 5 -X GET \
     -H "Content-Type: application/json" \
-    http://${METABASE_HOST}:${METABASE_PORT}/api/session/properties \
+    http://${MB_HOST}:${MB_PORT}/api/session/properties \
     | jq -r '.["setup-token"]'
 )
 
-MB_TOKEN=$(curl -s -X POST \
+curl -s -X POST \
     -H "Content-type: application/json" \
-    http://${METABASE_HOST}:${METABASE_PORT}/api/setup \
+    http://${MB_HOST}:${MB_PORT}/api/setup \
     -d '{
     "token": "'${SETUP_TOKEN}'",
     "user": {
-        "email": "'${METABASE_ADMIN_EMAIL}'",
-        "first_name": "Metabase",
-        "last_name": "Admin",
-        "password": "'${METABASE_ADMIN_PASSWORD}'"
+        "email": "'${MB_ADMIN_EMAIL}'",
+        "first_name": "'${MB_ADMIN_FIRST_NAME}'",
+        "last_name": "'${MB_ADMIN_LAST_NAME}'",
+        "password": "'${MB_ADMIN_PASSWORD}'"
     },
     "prefs": {
         "allow_tracking": false,
         "site_name": "Bahmni Metabase"
     }
-}' | jq -r '.id')
-
+}'
 
 echo -e "\n Admin users created!"
--- bahmni-metabase/docker/scripts/metabase_init.sh
+++ bahmni-metabase/docker/scripts/metabase_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "⌚︎ Waiting for Metabase to start"
-while (! curl -s -m 5 http://${METABASE_HOST}:${METABASE_PORT}/api/session/properties -o /dev/null); do sleep 5; done
+echo "Waiting for Metabase to start"
+while (! curl -s -m 5 http://${MB_HOST}:${MB_PORT}/api/session/properties -o /dev/null); do sleep 5; done
 
 /app/scripts/create_admin.sh
