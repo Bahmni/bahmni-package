b3349eb978ec8c2e092cbb87488a63c501850628
Rahul Ramesh <121226043+rahu1ramesh@users.noreply.github.com>
2024-06-18 11:09:08 +0530

    [Rahul] | BAH-3969 | Add. Main Class Exceptions

--- bahmni-embedded-tomcat-10/src/main/java/org/bahmni/deployment/launch/Main.java
+++ bahmni-embedded-tomcat-10/src/main/java/org/bahmni/deployment/launch/Main.java
@@ -1,16 +1,18 @@
 package org.bahmni.deployment.launch;
 
+import jakarta.servlet.ServletException;
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.Context;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static java.lang.System.getenv;
 
 public class Main {
-    public static void main(String[] args) throws LifecycleException {
+    public static void main(String[] args) throws NumberFormatException, NullPointerException, IllegalArgumentException, ServletException, LifecycleException, IllegalStateException, FileNotFoundException, IOException{
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.parseInt(getenv("SERVER_PORT")));
         tomcat.setBaseDir(getenv("BASE_DIR"));
